d1361b08c00a85e4929fb33072c8da6946829ffa
bernierocks <kittywebworks@gmail.com>
2014-01-02 18:24:56 +0800

    Updates and fixes

--- 01-greatshape-complex/footer-inc.php
+++
@@ -1,9 +0,0 @@
-<div class="footer">
-  <p><a href="terms.php" title="TERM &amp; CONDITION" target="_blank">Terms &amp; Conditions</a><span>.</span> <a href="privacy.php" title="PRIVACY POLICY"  target="_blank">Privacy Policy</a><span>.</span> <a href="contact.php" title="CONTACT"  target="_blank">Contact</a><span>.</span> <a href="refundpolicy.php" title="refund"  target="_blank">Refund Policy</a></p>
-  <p>2013 &copy; <?php echo $productName ?> - All rights reserved</p>
-  <p><?php echo $companyName ?> <br><?php echo $midAddress ?>, <?php echo $midCity ?> <?php echo $midCountry ?>
-	<br>
-<?php echo $midEmail ?>
-<?php echo $midPhone ?>
-</p>
-</div>
--- 01-greatshape-complex/index.php
+++ 01-greatshape-complex/index.php
@@ -1,91 +1,90 @@
-<?php include('config.php'); ?>
-<!DOCTYPE HTML>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
-    <META HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE">
-    <META NAME="ROBOTS" CONTENT="ALL"> 
-    <META NAME="ROBOTS" CONTENT="INDEX,NOFOLLOW"> 
- <META NAME="ROBOTS" CONTENT="NOINDEX,FOLLOW"> 
- <META NAME="ROBOTS" CONTENT="NONE">
- <META NAME="GOOGLEBOT" CONTENT="NOARCHIVE">
-<title><?php echo $productName ?></title>
+<!DOCTYPE html>
+<html lang="en">
     
-    <!-- CSS -->
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
-    <link type="text/css" href="css/style.css" rel="stylesheet">
-    
-    <!-- JS -->
-    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-    <!--[if lt IE 9]>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.1/html5shiv.js"></script>
-    <![endif]-->
-    <!--[if lt IE 9]>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.1/html5shiv.js"></script>
-    <![endif]-->
-    <script type="text/javascript" src="script/jquery.ez-bg-resize.js"></script>
-    <script type="text/javascript" src="script/function.js"></script>
-</head>
-
-<body>
-
-
-    <header>
-        <div class="container"> 
-            <div class="row">  
-                <div class="col col-sm-12 col-lg-6"> 
-                    <img src="images/logo.png" alt="<?php echo $productName ?>" title="<?php echo $productName ?>"  />        
-                </div><!-- /.col6-->
-                <div class="col col-sm-12 col-lg-6">   
-                    <?php include('menu-inc.php')?>            
-                </div><!-- /.col6-->
-            </div><!-- /.row -->
-        </div><!-- /.container -->
+    <head>
+        <meta charset="utf-8">
+        <title>Bootstrap, from Twitter</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
         
-    
+        <!-- CSS -->
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/main.css" rel="stylesheet"/>
+        <link href="css/flat-ui.css" rel="stylesheet"/>
         
+        <!-- Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Josefin+Sans:700' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Lato:400,900' rel='stylesheet' type='text/css'>
         
-    </header><!-- /header -->
-
-	
+        <!-- JS -->
+        <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+        <!--[if lt IE 9]>
+            <script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.1/html5shiv.js"></script>
+        <![endif]-->
+        <!--[if lt IE 9]>
+            <script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.1/html5shiv.js"></script>
+        <![endif]-->
+    </head>
     
-    
-<div class="wrapper">
-
-<!--CONTENT SECTION-->
-<div class="content main-bg">
-    <div class="container">
-        <img src="images/text.png" /> 
-        <ul class="bullet">
-            <li>Specialized Diet Programs</li>
-            <li>Fitness Programs</li>
-            <li>Great Healthy Low Carb Recipes</li>
-            <li>Meal Plans</li>
-            <li>E-Magazine with motivational and informative articles</li>
-        </ul>
-        <a href="order-today.php" class="join-today col-orange" title="Join Today for only $<?php echo $rebillRate ?>/Mo.">Join Today for only $<?php echo $rebillRate ?>/Mo.</a>
-    </div>
-
-<div class="clearb"></div>
-
-    <div class="note">
-    <p class="col-brown"><strong>Health and fitness today have become a growing concern, not only in America but all over the world.</strong>
-    We've addressed this need and offers our clients a total program that will help them get and stay fit,
-    while helping them improve their quality of life. We've taken into account all the tools to motivate and
-    help our clients enjoy every step to fitness, health, and total well being.</p>
-    </div>
-
-</div>
-<!--/END CONTENT-->
-
-<div class="clearb h20"></div>
-
-<!--FOOTER SECTION-->
-<?php include('footer-inc.php')?>
-
-</div>
-<!--/END-->
-</body>
+    <body class="">
+        <header>   
+            <div class="container">   
+                <div class="row">  
+                    <div class="logo col col-sm-12 col-lg-6">   
+                        <a href="#" alt=""><h1><img alt="" src="images/logo.png"><span>GreatShape</span> Complex</h1></a>
+                    </div><!-- /.col6-->
+                    <div class="col col-sm-12 col-lg-6">   
+                        <ul class="nav nav-pills pull-right">
+                          <li class="active"><a href="#">Home</a></li>
+                          <li><a href="#">Order Today</a></li>
+                          <li><a href="#">Login</a></li>
+                        </ul>
+                    </div><!-- /.col6-->
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </header><!-- /header -->
+        
+        <section class="home">   
+            <div class="container">   
+                <div class="row"> 
+                    <div class="col col-sm-12 col-lg-5">
+                        <img class="pull-right" alt="" src="images/model.png">
+                    </div><!-- /.col5-->
+                    <div class="col col-sm-12 col-lg-7">   
+                        <h3>Take your first step towards a</h3>
+                        <h1>Heathier You</h1>
+                        <h3>Here's what you get when you join today!</h3>
+                        <ul class="unstyled">
+                            <li><img alt="" src="">Specialized Diet Programs</li>
+                            <li><img alt="" src="">Fitness Programs</li>
+                            <li><img alt="" src="">Great Healthy Low Carb Recipes</li>
+                            <li><img alt="" src="">Meal Plans</li>
+                            <li><img alt="" src="">E-Magazine with motivational and informative articles</li>
+                        </ul>
+                        <a href="#fakelink" class="cta btn btn-huge btn-primary btn-embossed mlm"><img alt="" src="images/button-icon.png">Join Today for only  $9.84/mo.</a>
+                    </div><!-- /.col7-->
+                </div><!-- /.row -->
+                <div class="row"> 
+                    <div class="col col-sm-12 col-lg-10 col-lg-offset-1">  
+                    <div class="alert alert-success">
+                    <h3>Health and fitness today have become a growing concern, not only in America but all over the world. </h3>
+                    <p>We've addressed this need and offers our clients a total program that will help them get and stay fit, while helping them improve their quality of life. We've taken into account all the tools to motivate and help our clients enjoy every step to fitness, health, and total well being.</p>
+                    </div>
+                    </div><!-- /.col10-->
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </section><!-- /.section -->
+        
+        <footer> 
+            <p class="text-center">
+                Terms & Conditions. Privacy Policy. Contact. Refund Policy<br>
+                2013 © NutraPerfect Diet - All rights reserved<br>
+                Ballusarce Limited  Karpenisiou, 9 Strovolos, Nicosia Cyprua<br>
+                support@nutraperfectdiet.com1-855-847-8512
+            </p>
+            
+        </footer><!-- /footer -->
+    </body>
 </html>
